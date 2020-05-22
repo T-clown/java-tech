989a8f85cc3e5478289de46b3c3f9b7860a52be0
YCKJ2717 <tianjiangnan@cloudwalk.cn>
2020-05-22 17:29:21 +0800

    ref

--- src/main/java/arithmetic/alibaba/ReverseLinkedList.java
+++ src/main/java/arithmetic/alibaba/ReverseLinkedList.java
@@ -29,21 +29,23 @@ public class ReverseLinkedList {
      * @param head
      * @param n
      */
-    private static Node deleteNode(Node head, int n) {
-        Node first = head;
-        Node second = head;
 
+    public static Node deleteNode(Node head, int n) {
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node first = dummy;
+        Node second = dummy;
+        // Advances first pointer so that the gap between first and second is n nodes apart
         for (int i = 1; i <= n + 1; i++) {
             first = first.next;
         }
-
+        // Move first to the end, maintaining the gap
         while (first != null) {
             first = first.next;
             second = second.next;
         }
         second.next = second.next.next;
-
-        return head;
+        return dummy.next;
     }
 
     /**
