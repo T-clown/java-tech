5797fbeb51f7d9c6175fcb643e15257e7bbaedce
YCKJ2717 <tianjiangnan@cloudwalk.cn>
2020-06-04 19:51:17 +0800

    ref

--- src/main/java/concurrent/JucApp.java
+++ src/main/java/concurrent/JucApp.java
@@ -1,7 +1,18 @@
 package concurrent;
 
-public class JucApp {
-    public static void main(String[] args) {
+import java.util.concurrent.locks.LockSupport;
 
+public class JucApp {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(() -> {
+            System.out.println("LockSupport.park");
+            LockSupport.park();
+            System.out.println("LockSupport.unpark");
+        });
+        LockSupport.unpark(t);
+        t.start();
+        t.join();
+        System.out.println("main thread unpark t");
+        LockSupport.unpark(t);
     }
 }
