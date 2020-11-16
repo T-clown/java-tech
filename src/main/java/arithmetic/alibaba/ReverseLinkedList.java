f063b5840d35c436abb30d8af6d4ef637ac073f2
田江南 <tianjiangnan@cloudwalk.com>
2020-11-16 17:39:43 +0800

    ref

--- src/main/java/arithmetic/alibaba/ReverseLinkedList.java
+++ src/main/java/arithmetic/alibaba/ReverseLinkedList.java
@@ -39,6 +39,7 @@ public class ReverseLinkedList {
         while (first != null) {
             first = first.next;
             if (n < 1 && first != null) {
+                //让first指针先遍历n个节点，second指针才开始走，当first为空，second即为倒数第n个节点
                 second = second.next;
             }
             --n;
