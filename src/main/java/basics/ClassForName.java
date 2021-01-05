5f5865e3dd631dd4d2baabb6681a260c3f827ea7
田江南 <tianjiangnan@cloudwalk.com>
2021-01-05 17:17:35 +0800

    ref

---
+++ src/main/java/basics/ClassForName.java
@@ -0,0 +1,17 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ClassForName {
+    static {
+        System.out.println("执行静态代码块");
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        //会执行静态代码块
+        Class<?> clazz = Class.forName("basics.ClassForName");
+        //不会执行静态代码块
+        Class<?> clazz2 = ClassLoader.getSystemClassLoader().loadClass("basics.ClassForName");
+    }
+}
